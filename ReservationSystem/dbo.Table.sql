fa522ee67c166f64c4c678d5cde6988c389b6bd2
lsiemers <lsiemers@missouriwestern.edu>
2023-08-04 16:53:41 -0500

    Creating a local SQL Database which is embeeded in the projects

---
+++ ReservationSystem/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [First Name] VARCHAR(50) NULL, 
+    [Last Name] VARCHAR(50) NULL, 
+    [Phone Number] VARCHAR(50) NULL, 
+    [Size] INT NULL
+)
